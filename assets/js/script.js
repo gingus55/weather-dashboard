85235ac1973b72d6e607ec093980a979ecd0a6d1
gingus55 <gingus55@hotmail.com>
2021-10-16 19:18:27 +0100

    current weather renders and clears old data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 console.log("howdy planet");
 const myAPIKey = "f23ee9deb4e1a7450f3157c44ed020e1";
+const currentWeather = $("#current-weather");
 
 const getHistoryFromLS = function () {
   console.log("retrieved history");
@@ -21,15 +22,27 @@ const renderSearchHistory = function () {
   constructSearchHistory();
 };
 
-const constructTodaysWeather = function () {
-  console.log("todays weather constructed");
+const constructTodaysWeather = function (data) {
+  return `<div>
+    <h2>${data.name} AND ${data.dt}
+        <img src="http://openweathermap.org/img/wn/${data.weather.icon}@2x.png">
+    </h2>
+
+    <ul class="list-group-flush">
+        <li class="custom">Temperature: ${data.main.temp}</li>
+        <li class="custom">Humidity: ${data.main.humidity}</li>
+        <li class="custom">Wind Speed: ${data.wind.speed}</li>
+        <li class="custom">UV</li>
+      </ul>
+  </div>`;
 };
 
-const renderTodaysWeather = function () {
+const renderTodaysWeather = function (data) {
   console.log("todays weather rendered");
   //   construct todays weather and append
-  $("#current-weather").empty();
-  constructTodaysWeather();
+  currentWeather.empty();
+  const topBlock = constructTodaysWeather(data);
+  currentWeather.append(topBlock);
 };
 
 const constructForecast = function () {
@@ -48,7 +61,7 @@ const handleClick = function (event) {
   //   get value from search input
   const searchBox = $("#city-input");
   const city = searchBox.val();
-  console.log(city);
+  //   console.log(city);
   // validate value
   if (!city) {
     const error = document.createElement("p");
@@ -59,7 +72,7 @@ const handleClick = function (event) {
     const myUrl =
       `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=` +
       myAPIKey;
-    console.log(myUrl);
+    // console.log(myUrl);
 
     // handle response
     const handleResponse = function (response) {
@@ -68,7 +81,7 @@ const handleClick = function (event) {
     // handle data
 
     const handleData = function (data) {
-      console.log(data);
+      //   console.log(data);
       //   render todays weather
       renderTodaysWeather(data);
       // render forecast
@@ -82,14 +95,14 @@ const handleClick = function (event) {
   //   fetch current data for city
 
   //  fetch forecast for city
-};;
+};
 
 const onLoad = function () {
-  //   render search history
-  renderSearchHistory();
-  // render top search history details
-  renderTodaysWeather();
-  renderForecast();
+  //   //   render search history
+  //   renderSearchHistory();
+  //   // render top search history details
+  //   renderTodaysWeather();
+  //   renderForecast();
 
   $("#submit").on("click", handleClick);
 };

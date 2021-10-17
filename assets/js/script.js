1addbf077097f788e93d9cb6eee9a588e9fdd3f5
gingus55 <gingus55@hotmail.com>
2021-10-17 18:19:07 +0100

    stopped duplicate buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,7 @@ const myAPIKey = "f23ee9deb4e1a7450f3157c44ed020e1";
 const currentWeather = $("#current-weather");
 
 const constructSearchHistory = function (array) {
-  console.log("history constructed");
-  //   construct  and append history buttons
-  array.forEach(element => {
+  array.forEach((element) => {
     historyButton = `<li><button id="search-btn">${element}</button></li>`;
     const searchHist = $("#search-history");
     searchHist.append(historyButton);
@@ -13,7 +11,6 @@ const constructSearchHistory = function (array) {
 };
 
 const renderSearchHistory = function () {
-  console.log("history rendered");
   //   get history data from LS
   const array = getFromLocalStorage();
   //construct history button and append to search history
@@ -21,13 +18,11 @@ const renderSearchHistory = function () {
 };
 
 const constructTodaysWeather = function (data) {
-  // console.log(data.weather[0].icon);
-    const date = moment($(data.dt)).format("DD-MM-YYYY");
+  const date = moment($(data.dt)).format("DD-MM-YYYY");
   return `<div>
     <h2>${data.name} : ${date}
         <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}.png">
     </h2>
-
     <ul class="list-group-flush">
         <li class="custom">Temperature: ${data.main.temp}</li>
         <li class="custom">Humidity: ${data.main.humidity}</li>
@@ -38,8 +33,6 @@ const constructTodaysWeather = function (data) {
 };
 
 const renderTodaysWeather = function (data) {
-  console.log("todays weather rendered");
-  //   construct todays weather and append
   currentWeather.empty();
   const topBlock = constructTodaysWeather(data);
   currentWeather.append(topBlock);
@@ -55,10 +48,10 @@ const renderForecast = function () {
   constructForecast();
 };
 
-const getFromLocalStorage = function(){
-    const searchHistory = JSON.parse(localStorage.getItem("history")) || [];
-    return searchHistory;
-}
+const getFromLocalStorage = function () {
+  const searchHistory = JSON.parse(localStorage.getItem("history")) || [];
+  return searchHistory;
+};
 
 const handleClick = function (event) {
   event.preventDefault();
@@ -66,46 +59,40 @@ const handleClick = function (event) {
   //   get value from search input
   const searchBox = $("#city-input");
   const city = searchBox.val();
-  //   console.log(city);
   // validate value
   if (!city) {
     const error = document.createElement("p");
     error.textContent = "please enter a valid city";
     $("#error-response").append(error);
   } else {
-    //   create Url
     const myUrl =
       `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=` +
       myAPIKey;
-    
-    // render search history
-
-    // handle response
 
     const handleResponse = function (response) {
-        return response.json();
-      
+      return response.json();
     };
-    // handle data
-    
-    const handleData = function (data) {
-    //   validate data
-    if(data.cod==200){
-        // save city into local storage
-    const searched = getFromLocalStorage();
-    searched.push(data.name);
-    localStorage.setItem("history", JSON.stringify(searched));
-    // render search history
 
-       //   render todays weather
-      renderTodaysWeather(data);
-      // render forecast
-      renderForecast(data);
-    } else {
-      const error = document.createElement("p");
-      error.textContent = "please enter a valid city";
-      $("#error-response").append(error);
-    };
+    const handleData = function (data) {
+      //   validate data
+      if (data.cod == 200) {
+        // save city into local storage
+        const searched = getFromLocalStorage();
+        if (!searched.includes(data.name)) {
+          searched.push(data.name);
+          localStorage.setItem("history", JSON.stringify(searched));
+        }
+        // render search history
+
+        //   render todays weather
+        renderTodaysWeather(data);
+        // render forecast
+        renderForecast(data);
+      } else {
+        const error = document.createElement("p");
+        error.textContent = "please enter a valid city";
+        $("#error-response").append(error);
+      }
     };
     // make fetch request
 
